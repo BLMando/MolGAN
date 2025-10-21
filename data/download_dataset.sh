984fabde9e6ad3a14b09c81a46cb3e4653513da0
Mattia Mandorlini <mando3228@gmail.com>
2025-10-21 11:08:00 +0200

    refactor: Remove unused visualization tests and molecular metric utilities
    - Deleted `test_visualization.py` as it was no longer needed.
    - Removed `molecular_metrics.py`, `sparse_molecular_dataset.py`, `trainer.py`, `utils.py` due to redundancy and lack of integration with the current codebase.
    - Cleaned up the utils directory by eliminating obsolete files that were not utilized in the main application.

--- data/download_dataset.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-curl -L -O http://deepchem.io.s3-website-us-west-1.amazonaws.com/datasets/gdb9.tar.gz
-tar xvzf gdb9.tar.gz
-rm gdb9.tar.gz
-
-curl -L -O https://github.com/gablg1/ORGAN/raw/master/organ/NP_score.pkl.gz
-curl -L -O https://github.com/gablg1/ORGAN/raw/master/organ/SA_score.pkl.gz
